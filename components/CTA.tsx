93acd8906b3c1d98e1b9976376243bd5458d9123
gaurav0973 <codejourneypanda@gmail.com>
2025-08-22 18:42:00 +0530

    built the home page

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,6 +1,33 @@
+import Image from "next/image"
+import Link from "next/link"
+
 function CTA() {
   return (
-    <div>CTA</div>
+        <section className="cta-section">
+            <div className="cta-badge">Start learning Your way</div>
+            <h2 className="text-3xl font bold">
+                Build and Personalize  Learning Companion
+            </h2>
+            <p>
+                Pick a name, subject, voice, & personality - and start your learning through voice conversation that feels natural and fun 
+            </p>
+            <Image
+                src="images/cta.svg"
+                alt="cta"
+                width={362}
+                height={232}
+            />
+            <button className="btn-primary">
+                <Image
+                    src="/icons/plus.svg"
+                    alt="plus"
+                    width={12} height={12}
+                />
+                <Link href="/companions/new">
+                    <p>Buid a new Companion</p>
+                </Link>
+            </button>
+        </section>
   )
 }
 export default CTA
