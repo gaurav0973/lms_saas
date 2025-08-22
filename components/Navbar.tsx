ab3d250855087c068b04afadee6ca850daa906fc
gaurav0973 <codejourneypanda@gmail.com>
2025-08-22 21:54:55 +0530

    added clerk authentication in the application

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 import Navitems from "./Navitems"
-
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 function Navbar() {
   return (
@@ -17,7 +22,15 @@ function Navbar() {
         </Link>
             <div className="flex items-center gap-8">
                 <Navitems/>
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin"> SignIn</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
+                
             </div>
     </nav>
   )
