edd429d7702b12d79c3ada1c1e186fd44fca943f
gaurav0973 <codejourneypanda@gmail.com>
2025-08-23 16:35:50 +0530

    add subject filter feature

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,6 +1,60 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+import { subjects } from "@/constants";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"; // ✅ use your shadcn/ui wrapper
+
 function SubjectFilter() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("subject") || "";
+
+  const [subject, setSubject] = useState(query);
+
+  useEffect(() => {
+    let newUrl = "";
+
+    if (subject === "all") {
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["subject"],
+      });
+    } else {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "subject",
+        value: subject,
+      });
+    }
+
+    router.push(newUrl, { scroll: false });
+  }, [subject]);
+
   return (
-    <div>SubjectFilter</div>
-  )
+    <Select value={subject} onValueChange={(val) => setSubject(val)}>
+      <SelectTrigger className="input capitalize">
+        <SelectValue placeholder="Subject" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">All Subjects</SelectItem>
+        {subjects.map((subject) => (
+          <SelectItem key={subject} value={subject} className="capitalize">
+            {subject}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 }
-export default SubjectFilter
+
+export default SubjectFilter;
