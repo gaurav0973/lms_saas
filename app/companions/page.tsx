7b4062787536b9cd6e21341b7ebba8736a7cc944
gaurav0973 <codejourneypanda@gmail.com>
2025-08-23 15:45:12 +0530

    add display all companions feature

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,35 @@
-function CompanionsLibrary() {
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+
+async function CompanionsLibrary({searchParams} : SearchParams) {
+  
+  const filters = await searchParams;
+  console.log("search params : ", filters)
+
+  const subject = filters?.subject ? filters.subject : ""
+  const topic = filters?.topic ? filters.topic : ""
+
+
+  const companions = await getAllCompanions({subject, topic})
+  console.log(companions)
+
   return (
-    <div>CompanionsLibrary</div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col ">
+          <h1>Companion Library</h1>
+          <div className="flex gap-4">Filters</div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id}
+          {... companion}
+          color={getSubjectColor(companion.subject)}          
+          />
+        ))}
+
+      </section>
+    </main>
   )
 }
 export default CompanionsLibrary
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server"
 import { CreateSupabaseClient } from "../supabase"
 
+
 export const createCompanion = async (formData :CreateCompanion) => {
     
     // user 
@@ -23,5 +24,37 @@ export const createCompanion = async (formData :CreateCompanion) => {
         throw new Error(error?.message || "Failed to load companion")
     }
     return data[0];
+
+}
+
+
+
+export const getAllCompanions = async ({limit =10, page=1, subject, topic} : GetAllCompanions) => {
+
+    const supabase = CreateSupabaseClient()
+
+    let query = supabase.from("companions").select()
+
+    if(subject && topic){
+        query = query.ilike("subject", `%${subject}%`)
+                    .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+    }
+    else if(subject){
+        query = query.ilike("subject", `%${subject}%`)
+    }
+    else if(topic){
+        query = query.or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+    }
+
+
+    query = query.range((page-1)*limit, page*limit-1)
+    const {data : companions, error} = await query
+
+    if(error)
+        throw new Error(error?.message || "Failed to get All companions")
+
+    return companions
+
+
 
 }
